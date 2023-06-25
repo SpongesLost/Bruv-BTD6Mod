f22a83ce8d8c8a8cb57226caeaa834549fad6474
SpongesLost <137681215+SpongesLost@users.noreply.github.com>
2023-06-25 12:04:51 +0200

    Update ModHelperData.cs
    Changed some of the values in the file

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,10 @@ namespace Bruv;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "37.3";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "Bruv";
 
-    public const string Description = "An empty mod";
+    public const string Description = "A mod filled with random stuff";
 
     public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
